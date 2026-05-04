af9825d0fd758a1795967796b0fd778e9aa7fa1e
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 18:08:59 -0400

    Implementación de flyway en properties e implementación de scripts mysql

---
+++ 06-consultas/src/main/resources/db-migration/V2_insert_consultas.sql
@@ -0,0 +1,5 @@
+INSERT INTO consultas(nombre_box, piso, tipo_box, disponible)
+VALUES
+('Box cardiologia',3, 'Cardiología', TRUE),
+('Box ginecología',2, 'Ginecología', TRUE),
+('Box traumatología',4, 'Traumatología', TRUE);
